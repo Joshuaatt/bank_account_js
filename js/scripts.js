5c0b8c20e28018a8d3222e2661c09723bdd2ca52
Joshua Atteberry and Lee Jorgensen <leejorgensen22@gmail.com>
2015-02-18 11:22:47 -0800

    add functioning deposit/withdraw

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,26 @@ $(function() {
     event.preventDefault();
 
     var inputName = $("input#name").val();
-    var initialDeposit = $("input#initial-deposit").val();
+    var initialDeposit = parseFloat($("input#initial-deposit").val());
 
+    var newBankAccount = Object.create(BankAccount);
+      newBankAccount.name = inputName;
+      newBankAccount.balance = initialDeposit;
 
+      $("form#withdraw-deposit").submit(function(event) {
+        event.preventDefault();
+
+        var deposit = parseFloat($("input#deposit").val());
+        var withdraw = parseFloat($("input#withdraw").val());
+        newBankAccount.deposit(deposit);
+        newBankAccount.withdraw(withdraw);
+        $("#response").text(newBankAccount.balance);
+        var deposit = $("input#deposit").val("");
+        var withdraw = $("input#withdraw").val("");
+      });
+
+     $("#response").text(newBankAccount.balance);
+     var inputName = $("input#name").val("");
+     var initialDeposit = $("input#initial-deposit").val("");
   });
 });
